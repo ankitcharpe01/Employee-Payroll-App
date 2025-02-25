ef953e8331ec6e93e66221329c2a65a158a4f14e
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-02-25 14:04:08 +0530

    [Ankit Charpe]Add.UC2 - REST Controller with HTTP Methods

--- src/main/java/com/example/springemployeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/example/springemployeepayrollapp/controller/EmployeePayrollController.java
@@ -4,12 +4,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.*;
+
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/employee")
 public class EmployeePayrollController {
 
     @GetMapping("/welcome")
     public String welcomeMessage() {
         return "Welcome to Employee Payroll App!";
+    }
+
+    @PostMapping("/add")
+    public String addEmployee() {
+        return "Employee added successfully!";
+    }
+
+    @PutMapping("/update")
+    public String updateEmployee() {
+        return "Employee updated successfully!";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteEmployee() {
+        return "Employee deleted successfully!";
     }
 }
