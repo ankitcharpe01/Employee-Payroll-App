e5648c6b022b2b72dfb0b85819c9ed6968596827
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-02-26 22:39:07 +0530

    [Ankit Charpe]Add.Added UC6 - Used Lombok for Getters and Setters

--- src/main/java/com/example/springemployeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/example/springemployeepayrollapp/dto/EmployeeDTO.java
@@ -1,36 +1,15 @@
 package com.example.springemployeepayrollapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
     private String name;
     private long salary;
 
-    public EmployeeDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "EmployeeDTO{" +
-                "name='" + name + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
 }
 
